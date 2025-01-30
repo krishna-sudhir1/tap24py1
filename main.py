935cc9b26518afbaea119dd33b636173575b7df8
krishna-sudhir1 <krishna.sudhir@aig.com>
2025-01-30 19:29:50 +0000

    some more

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ n1 = 5
 
 #typecast
 print(h+str(n1))
+
+n2 = 3
+res = n1 + n2
+out = f"{n1} + {n2} = {n1 + n2}"
+print(out)
+
+
+if n1 < n2:
+    print("Lesser")
+    print("<"*6)
+elif n1 == n2:
+    print("Equal")
+    print('=====')
+else:
+    print("Greater")
+    print(">"*len("Greater"))
+
+f = 17.3
+print(type(f))
